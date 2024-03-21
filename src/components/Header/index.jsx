4b14ba74d3f328e8b08749cdc1cc503638440fab
Wesley Mattos <wesleymattos025@gmail.com>
2024-03-21 13:51:25 -0300

    add profile components

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,24 @@
-import { Container } from "./styles"
+import { Container, Profile, Logout } from "./styles"
+import { RiShutDownLine } from "react-icons/ri"
 
 export function Header() {
 
   return (
     <Container>
+      <Profile>
+        <img src="https://github.com/WesleyMattos1.png"
+          alt="Foto do usuário" />
 
+        <div>
+          <span>Bem-Vindo</span>
+          <strong>Wesley Mattos</strong>
+        </div>
+
+      </Profile>
+
+      <Logout>
+        <RiShutDownLine />
+      </Logout>
     </Container>
   )
 }
--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,7 +1,8 @@
 import { Container } from './styles'
+
 import { Header } from '../../components/Header'
 import { Button } from '../../components/Button'
-
+import { Section } from '../../components/Section'
 export function Details() {
 
 
@@ -9,6 +10,15 @@ export function Details() {
 
     <Container>
       <Header />
+      <Section title='Links úteis'>
+        <ul>
+          <li>Item 1</li>
+          <li>Item 2</li>
+          <li>Item 3</li>
+        </ul>
+      </Section>
+
+
 
       <Button title='Voltar' />
     </Container>
